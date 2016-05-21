9e17a5c0e6e0d79e01d85d01accb9617c723acf4
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-21 08:41:44 -0600

    Moved in_place insertion method to the bottom since it is not primary

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -5,19 +5,6 @@ def sort(array)
   swapping_insertion_sort(array)
 end
 
-def in_place_insertion_sort(array)
-  1.upto(array.size - 1) do |index|
-    to_insert = array[index]
-    location = index - 1
-    while location >= 0 && array[location] > to_insert
-      array[location + 1] = array[location]
-      location -= 1
-    end
-    array[location + 1] = to_insert
-  end
-  return array
-end
-
 def swapping_insertion_sort(array)
   return array if array.size <= 1
   1.upto(array.size - 1) do |index|
@@ -28,6 +15,19 @@ def swapping_insertion_sort(array)
       array[location - 1] = holding
       location -= 1
     end
+  end
+  return array
+end
+
+def in_place_insertion_sort(array)
+  1.upto(array.size - 1) do |index|
+    to_insert = array[index]
+    location = index - 1
+    while location >= 0 && array[location] > to_insert
+      array[location + 1] = array[location]
+      location -= 1
+    end
+    array[location + 1] = to_insert
   end
   return array
 end
