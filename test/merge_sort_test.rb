386adcfb658bf06e4d18fb958deaa7312f3e1c69
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-21 08:29:08 -0600

    Added additional tests to cover edge cases

--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -13,9 +13,19 @@ class MergeSortTest < Minitest::Test
     assert_equal [1], result
   end
 
+  def test_it_returns_an_empty_array_if_given_an_empty_array
+    result = @merge.sort([])
+    assert_equal [], result
+  end
+
   def test_it_can_sort_two_items
     result = @merge.sort([4, 1])
     assert_equal [1, 4], result
+  end
+
+  def test_it_can_sort_an_odd_number_of_items
+    result = @merge.sort([0, 4, 3, 1, 2])
+    assert_equal [0, 1, 2, 3, 4], result
   end
 
   def test_it_can_sort_a_larger_array
@@ -28,5 +38,24 @@ class MergeSortTest < Minitest::Test
     assert_equal ["a", "b", "c", "d"], result
   end
 
+  def test_it_will_return_an_already_sorted_array
+    result = @merge.sort([1, 2, 3, 4, 5])
+    assert_equal [1, 2, 3, 4, 5], result
+  end
+
+  def test_it_can_sort_a_reverse_sorted_array
+    result = @merge.sort([5, 4, 3, 2, 1])
+    assert_equal [1, 2, 3, 4, 5], result
+  end
+
+  def test_it_can_sort_multichar_strings
+    result = @merge.sort(["bde", "abc", "abd", "aac"])
+    assert_equal ["aac", "abc", "abd", "bde"], result
+  end
+
+  def test_it_can_sort_multidigit_numbers
+    result = @merge.sort([13, 455, 06, 41])
+    assert_equal [06, 13, 41, 455], result
+  end
 
 end
