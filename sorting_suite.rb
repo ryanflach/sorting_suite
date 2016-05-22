ee181387829b2131b6d3ecce9fc0795ee8483f0c
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-22 09:14:01 -0600

    Module to store functionality of all classes created

---
+++ sorting_suite.rb
@@ -0,0 +1,20 @@
+require './lib/bubble_sort'
+require './lib/insertion_sort'
+require './lib/merge_sort'
+require './lib/benchmark'
+
+module SortingSuite
+
+  class BubbleSort < BubbleSort
+  end
+
+  class InsertionSort < InsertionSort
+  end
+
+  class MergeSort < MergeSort
+  end
+
+  class Benchmark < Benchmark
+  end
+
+end
